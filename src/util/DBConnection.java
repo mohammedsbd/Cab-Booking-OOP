b5f2dd7703d96ba2b4460d3b75150abb4e3bc38b
mohammedsbd <mohammed1112ok@gmail.com>
2025-07-31 03:25:41 -0700

    update db connection by adding db properties in gitignore

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -1,27 +1,42 @@
 package util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/cbs2";
-    private static final String USER = "cbs";
-    private static final String PASSWORD = "123456";
+    private static String url;
+    private static String user;
+    private static String password;
 
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("✅ MySQL JDBC Driver Registered!");
+
+            // Load properties from file
+            Properties props = new Properties();
+            FileInputStream fis = new FileInputStream("src/util/db.properties");
+            props.load(fis);
+
+            url = props.getProperty("db.url");
+            user = props.getProperty("db.user");
+            password = props.getProperty("db.password");
+
         } catch (ClassNotFoundException e) {
-            System.out.println("❌ MySQL JDBC Driver NOT found!");
+            System.out.println("❌ JDBC Driver not found!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("❌ Failed to load db.properties");
             e.printStackTrace();
         }
     }
 
     public static Connection getConnection() throws SQLException {
-        // Return a NEW connection every time this method is called
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(url, user, password);
     }
 }
