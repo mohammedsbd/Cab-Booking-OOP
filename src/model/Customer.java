e68abcb8994918cc8a9860785ee7fcabbda65954
mohammedsbd <mohammed1112ok@gmail.com>
2025-08-28 09:29:50 -0700

    udpadet

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -326,3 +326,659 @@ public class Customer implements Displayable {
         System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
     }
 }
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
+package model;
+
+import Interfaces.Displayable;
+
+public class Customer implements Displayable {
+    private int id;
+    private String name;
+    private String phone;
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public Customer(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Customer ID: " + id + ", Name: " + name + ", Phone: " + phone);
+    }
+}
